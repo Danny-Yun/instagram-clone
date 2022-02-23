7232b277e9b322e33303daebd6ad3e7e634476ad
Danny-Yun <lacvert13@gmail.com>
2022-02-23 13:41:02 +0900

    안 쓰는 라우트 정리

--- lib/router/RoutePage.dart
+++ lib/router/RoutePage.dart
@@ -1,6 +1,5 @@
 import 'package:get/get.dart';
 import 'package:instagram_clone/binding/NavigationControllerBinding.dart';
-import 'package:instagram_clone/screen/HomeScreen.dart';
 import 'package:instagram_clone/screen/NavigationScreen.dart';
 import 'package:instagram_clone/screen/UploadScreen.dart';
 
@@ -8,21 +7,13 @@ class RoutePage {
   static movePage(String pageName) => Get.toNamed(pageName);
 
   static const String NAVIGATION_ROUTE = "/navigation";
-  static const String HOME_ROUTE = "/home";
   static const String UPLOAD_ROUTE = "/upload";
-  static const String SEARCH_SCREEN = "/searchScreen";
-  static const String ACTIVE_SCREEN = "/activeScreen";
-  static const String PROFILE_SCREEN = "/profileScreen";
 
   static final List<GetPage> getPageList = [
     GetPage(
       name: NAVIGATION_ROUTE,
       page: () => NavigationScreen(),
       binding: NavigationControllerBinding(),
-    ),
-    GetPage(
-      name: HOME_ROUTE,
-      page: () => HomeScreen(),
     ),
     GetPage(
       name: UPLOAD_ROUTE,
